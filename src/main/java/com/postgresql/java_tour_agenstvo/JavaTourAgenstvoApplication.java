683e0faf37af06321daea12f72c57be5797400ed
Ermekov Erasyl <ermekov_erasil@gmail.com>
2024-12-16 18:22:43 +0500

    one

---
+++ src/main/java/com/postgresql/java_tour_agenstvo/JavaTourAgenstvoApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql.java_tour_agenstvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTourAgenstvoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTourAgenstvoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/postgresql/java_tour_agenstvo/JavaTourAgenstvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgresql.java_tour_agenstvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTourAgenstvoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
